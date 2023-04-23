9d5808771df4242ce52b19097b357d09a0ad1269
ndk2509 <dangkyqt2509@gmail.com>
2023-04-23 10:00:00 +0700

    feat: add bottom buttons

--- app/src/main/java/com/example/foodomer/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/foodomer/ui/screens/HomeScreen.kt
@@ -2,10 +2,13 @@ package com.example.foodomer.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.FabPosition
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -13,6 +16,8 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.foodomer.R
 import com.example.foodomer.ui.AppViewModelProvider
+import com.example.foodomer.ui.components.core.AddNewFoodButton
+import com.example.foodomer.ui.components.core.FoodomerButton
 import com.example.foodomer.ui.components.core.Header
 import com.example.foodomer.ui.components.home.HorizontalFoodList
 import com.example.foodomer.ui.components.welcome.AbsoluteRow
@@ -26,28 +31,38 @@ fun HomeScreen(
     viewModel: HomeViewModel = viewModel(factory = AppViewModelProvider.Factory),
 ) {
     val foods by viewModel.foodList.collectAsState(initial = emptyList())
-
-    Column(
-        modifier = Modifier.fillMaxSize().padding(top = DEFAULT_PADDING, start = DEFAULT_PADDING, end = DEFAULT_PADDING)
-    ) {
-        Box(
-            modifier = Modifier.fillMaxHeight(0.3f)
-        ) {
-            Header(onClickMenu = {}, onCLickSearch = {})
-            AbsoluteRow(
-                offsetY = DEFAULT_PADDING,
+    Scaffold(
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = { AddNewFoodButton(onClickToClose = {}, onClickToOpen = {}) },
+        isFloatingActionButtonDocked = true,
+        bottomBar = {
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(bottom = 10.dp),
                 horizontalArrangement = Arrangement.Center
             ) {
-                Image(
-                    painterResource(R.drawable.banner_1),
-                    "",
-                    modifier =
-                    Modifier
-                        .width(200.dp)
-                        .aspectRatio(1f)
-                )
+                FoodomerButton{}
             }
+        },
+        backgroundColor = Color.White,
+        drawerBackgroundColor = Color.White
+    ) { padding ->
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(top = DEFAULT_PADDING, start = DEFAULT_PADDING, end = DEFAULT_PADDING)
+        ) {
+            Box(
+                modifier = Modifier.fillMaxHeight(0.3f)
+            ) {
+                Header(onClickMenu = {}, onCLickSearch = {})
+                AbsoluteRow(
+                    offsetY = DEFAULT_PADDING, horizontalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painterResource(R.drawable.banner_1), "", modifier = Modifier.width(200.dp).aspectRatio(1f)
+                    )
+                }
+            }
+            HorizontalFoodList(foods)
         }
-        HorizontalFoodList(foods)
     }
 }

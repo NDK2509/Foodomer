1205647c82086b67dbd4991d08aa07d5b4bd1ba0
ndk2509 <dangkyqt2509@gmail.com>
2023-05-20 19:15:05 +0700

    feat: add the new Bottom bar

--- app/src/main/java/com/example/foodomer/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/foodomer/ui/screens/HomeScreen.kt
@@ -4,109 +4,58 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.FabPosition
-import androidx.compose.material.Scaffold
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.foodomer.R
 import com.example.foodomer.ui.AppViewModelProvider
-import com.example.foodomer.ui.components.core.AddNewFoodButton
-import com.example.foodomer.ui.components.core.FoodomerButton
 import com.example.foodomer.ui.components.core.Header
-import com.example.foodomer.ui.components.home.CreationOptionsOverLay
 import com.example.foodomer.ui.components.home.HorizontalFoodList
-import com.example.foodomer.ui.components.home.Option
 import com.example.foodomer.ui.components.welcome.AbsoluteRow
 import com.example.foodomer.ui.theme.DEFAULT_PADDING
 import com.example.foodomer.ui.viewmodels.HomeViewModel
 
-@Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun HomeScreen(
     navController: NavController? = null,
     viewModel: HomeViewModel = viewModel(factory = AppViewModelProvider.Factory),
 ) {
     val foods by viewModel.foodList.collectAsState(initial = emptyList())
-    var isVisibleOptionOverlay by remember { mutableStateOf(false) }
+    val permissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {}
 
-    Scaffold(
-        floatingActionButtonPosition = FabPosition.End,
-        floatingActionButton = {
-            Row(
-                modifier = Modifier.zIndex(100f)
-            ) {
-                AddNewFoodButton(
-                    onClickToOpen = {
-                        isVisibleOptionOverlay = true
-                    },
-                    onClickToClose = {
-                        isVisibleOptionOverlay = false
-                    },
-                )
-            }
-        },
-        isFloatingActionButtonDocked = true,
-        bottomBar = {
-            Row(
-                modifier = Modifier.fillMaxWidth().padding(bottom = 10.dp).zIndex(100f),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                FoodomerButton {
-                    navController?.navigate("randomizer")
-                }
-            }
-        },
-        backgroundColor = Color.White,
-        drawerBackgroundColor = Color.White
-    ) { padding ->
-        val permissionLauncher = rememberLauncherForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) {}
+    SideEffect {
+        permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        permissionLauncher.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    }
 
-        SideEffect {
-            permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
-            permissionLauncher.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        }
-
-        Box(
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
             modifier = Modifier.fillMaxSize()
+                .padding(top = DEFAULT_PADDING, start = DEFAULT_PADDING, end = DEFAULT_PADDING)
         ) {
-            Column(
-                modifier = Modifier.fillMaxSize()
-                    .padding(top = DEFAULT_PADDING, start = DEFAULT_PADDING, end = DEFAULT_PADDING)
+            Box(
+                modifier = Modifier.fillMaxHeight(0.3f)
             ) {
-                Box(
-                    modifier = Modifier.fillMaxHeight(0.3f)
+                Header(onClickMenu = {}, onCLickSearch = {})
+                AbsoluteRow(
+                    offsetY = DEFAULT_PADDING, horizontalArrangement = Arrangement.Center
                 ) {
-                    Header(onClickMenu = {}, onCLickSearch = {})
-                    AbsoluteRow(
-                        offsetY = DEFAULT_PADDING, horizontalArrangement = Arrangement.Center
-                    ) {
-                        Image(
-                            painterResource(R.drawable.banner_1), "", modifier = Modifier.width(200.dp).aspectRatio(1f)
-                        )
-                    }
+                    Image(
+                        painterResource(R.drawable.banner_1), "", modifier = Modifier.width(200.dp).aspectRatio(1f)
+                    )
                 }
-                HorizontalFoodList(foods)
             }
-            CreationOptionsOverLay(
-                options = listOf(
-                    Option("New Food") {
-                        navController?.navigate("create-food")
-                    },
-                    Option("New Category") {
-                        navController?.navigate("create-category")
-                    }
-                ),
-                isVisible = isVisibleOptionOverlay
-            )
+            HorizontalFoodList(foods)
         }
     }
 }

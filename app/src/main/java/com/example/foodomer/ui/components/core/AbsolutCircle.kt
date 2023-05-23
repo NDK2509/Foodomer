28fe5d2f8b62e48117740774a28d729b76560e1b
NDK2509 <Dangkyqt2509@gmail.com>
2023-05-23 22:05:53 +0700

    feat: update create category screen

---
+++ app/src/main/java/com/example/foodomer/ui/components/core/AbsolutCircle.kt
@@ -0,0 +1,33 @@
+package com.example.foodomer.ui.components.core
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.times
+
+@Composable
+fun AbsoluteCircle(
+    radius: Dp,
+    offsetX: Dp = 0.dp,
+    offsetY: Dp = 0.dp,
+    color: Color = Color.Black
+) {
+    Box(
+        modifier =
+            Modifier
+                .offset(offsetX, offsetY)
+                .width(2 * radius)
+                .aspectRatio(1f)
+                .clip(CircleShape)
+                .background(color, CircleShape)
+    ) {}
+}
--- app/src/main/java/com/example/foodomer/ui/viewmodels/CreateCategoryViewModel.kt
+++ app/src/main/java/com/example/foodomer/ui/viewmodels/CreateCategoryViewModel.kt
@@ -4,12 +4,47 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.foodomer.database.entities.Category
 import com.example.foodomer.database.repositories.CategoryRepository
+import com.example.foodomer.utils.capitalize
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-class CreateCategoryViewModel(val categoryRepository: CategoryRepository): ViewModel() {
-    fun save(name: String) {
+data class CategoryState(
+    val name: String = "",
+    var description: String = ""
+)
+class CreateCategoryViewModel(private val categoryRepository: CategoryRepository): ViewModel() {
+    private val _uiState: MutableStateFlow<CategoryState> = MutableStateFlow(CategoryState())
+    val uiSate = _uiState.asStateFlow()
+
+    fun setName(name: String) {
+        _uiState.value = _uiState.value.copy(name = name)
+    }
+
+    fun setDescription(description: String) {
+        _uiState.value = _uiState.value.copy(description = description)
+    }
+
+    fun save(): Boolean {
+        var result = true
         viewModelScope.launch {
-            categoryRepository.insert(Category(name = name, description = ""))
+            if (validate()) {
+                val categoryState = uiSate.value
+                categoryRepository.insert(Category(name = categoryState.name, description = categoryState.description))
+            } else {
+                result = false
+            }
         }
+
+        return result
+    }
+
+    private fun validate(): Boolean {
+        val categoryState = uiSate.value
+        val name = categoryState.name.trim()
+        val description = categoryState.description.trim()
+
+        _uiState.value = CategoryState(name = name.capitalize(), description = description)
+        return !(name == "" || description == "")
     }
 }
